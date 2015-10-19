facb5638a651c4801f9e0340a1a0ae346a539c37
Brayden Neale <brayden.neale@gmail.com>
2015-10-19 11:05:06 +0800

    Updating of models and schema

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -4,6 +4,8 @@ class Tutor < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_one :card, :as => :user
+
  # 	def self.search(search)
  # 		where("name like ?", "%#{search}%")
 	# end
