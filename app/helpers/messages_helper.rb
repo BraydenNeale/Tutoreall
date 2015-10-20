1e94922d0d6b358217df7530a69bf46fa28a500d
Brayden Neale <brayden.neale@gmail.com>
2015-10-20 21:44:29 +0800

    filtered participant options so that tutors can only talk to students

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -5,22 +5,14 @@ module MessagesHelper
 		# end
 		# s.html_safe
 		s = ''
-
-		# if is_tutor?
-		# if current_user.is_a? Tutor
-		# if is_tutor
-		# 	Student.all.each do |user|
-		# 		s << "<option value='#{user.id}'>#{user.display_name}</option>"
-		# 	end
-		# else
-		# 	Tutor.all.each do |user|
-		# 		s << "<option value='#{user.id}'>#{user.display_name}</option>"
-		# 	end
-		# end
-		# s.html_safe
-
-		Student.all.each do |user|
-			s << "<option value='#{user.id}'>#{user.display_name}</option>"
+		if is_tutor
+			Student.all.each do |user|
+				s << "<option value='#{user.id}'>#{user.display_name}</option>"
+			end
+		else
+			Tutor.all.each do |user|
+				s << "<option value='#{user.id}'>#{user.display_name}</option>"
+			end
 		end
 		s.html_safe
 	end
