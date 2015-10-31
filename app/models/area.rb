32ee88c5369cc264676e35737dcb2dacb042d16a
Brayden Neale <brayden.neale@gmail.com>
2015-10-31 21:27:11 +0800

    Added unique model verification to area/subject - also initial files to populate db

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,3 +1,5 @@
 class Area < ActiveRecord::Base
+		validates :name, presence: true, uniqueness: true
+		validates :code, presence: true, uniqueness: true
 	  has_and_belongs_to_many :tutors
 end
---
+++ db/sql-scripts/subject.rb
@@ -0,0 +1,8 @@
+Subject.create :name => "music", :code => "music"
+
+	Music
+	Business
+	Humanities
+	English
+	Science
+	Maths
