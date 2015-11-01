e222a9a166b97dc384f65c6f3d156ed03b9c67e6
Brayden Neale <brayden.neale@gmail.com>
2015-11-01 13:00:04 +0800

    Autocomplete working now

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,9 +1,7 @@
 class AreasController < ApplicationController
-	
+
 	def autocomplete
-    # Elasticsearch? - not used
     @areas = Area.all
-    # render json: @areas.where("LOWER(name) LIKE ?", "%#{params[:term].downcase}%").map(&:name)
-    render json: %w[foo bar]
+    render json: @areas.where("LOWER(name) LIKE ?", "%#{params[:term].downcase}%").map(&:name)
   end
 end
