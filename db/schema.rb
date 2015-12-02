87fa3892ae49a69a9dd9195022ad048de5ba9bcb
Brayden Neale <brayden.neale@gmail.com>
2015-12-02 20:00:20 +0800

    Some lesson view improvements - also added a weekday model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129125957) do
+ActiveRecord::Schema.define(version: 20151202115946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,6 +168,12 @@ ActiveRecord::Schema.define(version: 20151129125957) do
 
   add_index "tutors", ["email"], name: "index_tutors_on_email", unique: true, using: :btree
   add_index "tutors", ["reset_password_token"], name: "index_tutors_on_reset_password_token", unique: true, using: :btree
+
+  create_table "weekdays", force: :cascade do |t|
+    t.integer  "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "lessons", "students"
   add_foreign_key "lessons", "tutors"
