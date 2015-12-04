7f39d35d5d61ecf7aec38d8ffcfa49f39a5b3b51
Brayden Neale <brayden.neale@gmail.com>
2015-12-04 19:14:05 +0800

    devise migration for confirmation emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203120300) do
+ActiveRecord::Schema.define(version: 20151204111048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,8 +159,12 @@ ActiveRecord::Schema.define(version: 20151203120300) do
     t.string   "firstname"
     t.string   "lastname"
     t.date     "date_of_birth"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "tutors", ["confirmation_token"], name: "index_tutors_on_confirmation_token", unique: true, using: :btree
   add_index "tutors", ["email"], name: "index_tutors_on_email", unique: true, using: :btree
   add_index "tutors", ["reset_password_token"], name: "index_tutors_on_reset_password_token", unique: true, using: :btree
 
