cf91021e75d944590b1fcd1efaf8f79844e9ca58
Brayden Neale <brayden.neale@gmail.com>
2015-12-05 15:19:06 +0800

    Updated subject models - massive script overhaul for this

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,9 +1,10 @@
 class Subject < ActiveRecord::Base
-	validates :name, presence: true, uniqueness: true
-	validates :code, presence: true, uniqueness: true
+	validates :name, presence: true
+	validates :faculty, presence: true
+	validates :group, presence: true
 	has_and_belongs_to_many :tutors
 
-	def display_name
+  def display_name
     return "#{self.name}".titleize
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204130914) do
+ActiveRecord::Schema.define(version: 20151205045937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,9 +130,10 @@ ActiveRecord::Schema.define(version: 20151204130914) do
 
   create_table "subjects", force: :cascade do |t|
     t.string   "name"
-    t.string   "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "faculty"
+    t.string   "group"
   end
 
   create_table "subjects_tutors", id: false, force: :cascade do |t|
