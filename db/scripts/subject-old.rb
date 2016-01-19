96684293c4d1daa852a6cd4024ba8ad69f6f7ccf
Brayden Neale <brayden.neale@gmail.com>
2016-01-19 21:51:07 +0800

    reverted form style change - still broken on phone: added new subject script

---
+++ db/scripts/subject-old.rb
@@ -0,0 +1,178 @@
+# rails c 
+# load './db/scripts/subject.rb'
+
+# YEAR 12 SUBJECTS
+Subject.create :name => "mathematics applications", :faculty => "mathematics", :group => "year12"
+Subject.create :name => "mathematics essentials", :faculty => "mathematics", :group => "year12"
+Subject.create :name => "mathematics methods", :faculty => "mathematics", :group => "year12"
+Subject.create :name => "mathematics specialist", :faculty => "mathematics", :group => "year12"
+
+Subject.create :name => "accounting and finance", :faculty => "business and it", :group => "year12"
+Subject.create :name => "economics", :faculty => "business and it", :group => "year12"
+Subject.create :name => "applied information technology", :faculty => "business and it", :group => "year12"
+Subject.create :name => "engineering studies mechanical", :faculty => "business and it", :group => "year12"
+Subject.create :name => "career and enterprise", :faculty => "business and it", :group => "year12"
+
+Subject.create :name => "biology", :faculty => "science", :group => "year12"
+Subject.create :name => "human biology", :faculty => "science", :group => "year12"
+Subject.create :name => "chemistry", :faculty => "science", :group => "year12"
+Subject.create :name => "physics", :faculty => "science", :group => "year12"
+Subject.create :name => "integrated science", :faculty => "science", :group => "year12"
+
+Subject.create :name => "english (and foundational english)", :faculty => "english", :group => "year12"
+Subject.create :name => "english as an additional language/dialect", :faculty => "english", :group => "year12"
+Subject.create :name => "literature", :faculty => "english", :group => "year12"
+
+Subject.create :name => "geography", :faculty => "humanities", :group => "year12"
+Subject.create :name => "modern history", :faculty => "humanities", :group => "year12"
+Subject.create :name => "pyschology", :faculty => "humanities", :group => "year12"
+Subject.create :name => "politics and law", :faculty => "humanities", :group =>"year 12"
+
+Subject.create :name => "music", :faculty => "arts", :group => "year12"
+Subject.create :name => "drama", :faculty => "arts", :group => "year12"
+Subject.create :name => "dance", :faculty => "arts", :group => "year12"
+
+Subject.create :name => "chinese background language", :faculty => "language", :group => "year12"
+Subject.create :name => "chinese first language", :faculty => "language", :group => "year12"
+Subject.create :name => "chinese second language", :faculty => "language", :group => "year12"
+Subject.create :name => "french second language", :faculty => "language", :group => "year12"
+Subject.create :name => "german second language", :faculty => "language", :group => "year12"
+Subject.create :name => "japanese second language", :faculty => "language", :group => "year12"
+
+Subject.create :name => "materials design and technology - metal", :faculty => "other", :group => "year12"
+Subject.create :name => "materials design and technology - wood", :faculty => "other", :group => "year12"
+Subject.create :name => "outdoor recreation", :faculty => "other", :group => "year12"
+Subject.create :name => "physical education studies general", :faculty => "other", :group => "year12"
+Subject.create :name => "health studies", :faculty => "other", :group => "year12"
+Subject.create :name => "media production and analysis", :faculty => "other", :group => "year12"
+
+# YEAR 11 SUBJECTS
+Subject.create :name => "mathematics applications", :faculty => "mathematics", :group => "year11"
+Subject.create :name => "mathematics essentials", :faculty => "mathematics", :group => "year11"
+Subject.create :name => "mathematics methods", :faculty => "mathematics", :group => "year11"
+Subject.create :name => "mathematics specialist", :faculty => "mathematics", :group => "year11"
+
+Subject.create :name => "accounting and finance", :faculty => "business and it", :group => "year11"
+Subject.create :name => "economics", :faculty => "business and it", :group => "year11"
+Subject.create :name => "applied information technology", :faculty => "business and it", :group => "year11"
+Subject.create :name => "engineering studies mechanical", :faculty => "business and it", :group => "year11"
+Subject.create :name => "career and enterprise", :faculty => "business and it", :group => "year11"
+
+Subject.create :name => "biology", :faculty => "science", :group => "year11"
+Subject.create :name => "human biology", :faculty => "science", :group => "year11"
+Subject.create :name => "chemistry", :faculty => "science", :group => "year11"
+Subject.create :name => "physics", :faculty => "science", :group => "year11"
+Subject.create :name => "integrated science", :faculty => "science", :group => "year11"
+
+Subject.create :name => "english (and foundational english)", :faculty => "english", :group => "year11"
+Subject.create :name => "english as an additional language/dialect", :faculty => "english", :group => "year11"
+Subject.create :name => "literature", :faculty => "english", :group => "year11"
+
+Subject.create :name => "geography", :faculty => "humanities", :group => "year11"
+Subject.create :name => "modern history", :faculty => "humanities", :group => "year11"
+Subject.create :name => "pyschology", :faculty => "humanities", :group => "year11"
+Subject.create :name => "politics and law", :faculty => "humanities", :group =>"year 11"
+
+Subject.create :name => "music", :faculty => "arts", :group => "year11"
+Subject.create :name => "drama", :faculty => "arts", :group => "year11"
+Subject.create :name => "dance", :faculty => "arts", :group => "year11"
+
+Subject.create :name => "chinese background language", :faculty => "language", :group => "year11"
+Subject.create :name => "chinese first language", :faculty => "language", :group => "year11"
+Subject.create :name => "chinese second language", :faculty => "language", :group => "year11"
+Subject.create :name => "french second language", :faculty => "language", :group => "year11"
+Subject.create :name => "german second language", :faculty => "language", :group => "year11"
+Subject.create :name => "japanese second language", :faculty => "language", :group => "year11"
+
+Subject.create :name => "materials design and technology - metal", :faculty => "other", :group => "year11"
+Subject.create :name => "materials design and technology - wood", :faculty => "other", :group => "year11"
+Subject.create :name => "outdoor recreation", :faculty => "other", :group => "year12"
+Subject.create :name => "physical education studies general", :faculty => "other", :group => "year11"
+Subject.create :name => "health studies", :faculty => "other", :group => "year11"
+Subject.create :name => "media production and analysis", :faculty => "other", :group => "year11"
+Subject.create :name => "food science and technology", :faculty => "other", :group => "year11"
+
+# YEAR 10
+Subject.create :name => "mathematics", :faculty => "mathematics", :group => "year10"
+
+Subject.create :name => "accounting", :faculty => "business and it", :group => "year10"
+Subject.create :name => "law and commerce", :faculty => "business and it", :group => "year10"
+Subject.create :name => "law criminal investigation", :faculty => "business and it", :group => "year10"
+Subject.create :name => "computer game software design", :faculty => "business and it", :group => "year10"
+
+Subject.create :name => "general science", :faculty => "science", :group => "year10"
+Subject.create :name => "medical history", :faculty => "science", :group => "year10"
+Subject.create :name => "biology", :faculty => "science", :group => "year10"
+Subject.create :name => "chemistry", :faculty => "science", :group => "year10"
+Subject.create :name => "physics", :faculty => "science", :group => "year10"
+
+Subject.create :name => "english (and foundation english)", :faculty => "english", :group => "year10"
+Subject.create :name => "english as an additional language", :faculty => "english", :group => "year10"
+Subject.create :name => "literature", :faculty => "english", :group => "year10"
+
+Subject.create :name => "social sciences", :faculty => "humanities", :group => "year10"
+Subject.create :name => "geography", :faculty => "humanities", :group => "year10"
+Subject.create :name => "modern history", :faculty => "humanities", :group => "year10"
+Subject.create :name => "psychology", :faculty => "humanities", :group => "year10"
+Subject.create :name => "politics and law", :faculty => "humanities", :group => "year10"
+
+Subject.create :name => "music", :faculty => "arts", :group => "year10"
+Subject.create :name => "drama", :faculty => "arts", :group => "year10"
+Subject.create :name => "dance", :faculty => "arts", :group => "year10"
+
+Subject.create :name => "chinese background language", :faculty => "language", :group => "year10"
+Subject.create :name => "chinese first language", :faculty => "language", :group => "year10"
+Subject.create :name => "chinese second language", :faculty => "language", :group => "year10"
+Subject.create :name => "french second language", :faculty => "language", :group => "year10"
+Subject.create :name => "german second language", :faculty => "language", :group => "year10"
+Subject.create :name => "japanese second language", :faculty => "language", :group => "year10"
+
+Subject.create :name => "materials design and technology - metal", :faculty => "other", :group => "year10"
+Subject.create :name => "materials design and technology - wood", :faculty => "other", :group => "year10"
+Subject.create :name => "outdoor recreation", :faculty => "other", :group => "year10"
+Subject.create :name => "outdoor education", :faculty => "other", :group => "year10"
+Subject.create :name => "physical education studies general", :faculty => "other", :group => "year10"
+Subject.create :name => "health studies", :faculty => "other", :group => "year10"
+Subject.create :name => "media prodution and analysis", :faculty => "other", :group => "year10"
+Subject.create :name => "food science and technology", :faculty => "other", :group => "year10"
+Subject.create :name => "electronics", :faculty => "other", :group => "year10"
+Subject.create :name => "robotics and electronics", :faculty => "other", :group => "year10"
+Subject.create :name => "film production", :faculty => "other", :group => "year10"
+Subject.create :name => "photography", :faculty => "other", :group => "year10"
+Subject.create :name => "practical engineering", :faculty => "other", :group => "year10"
+
+# LOWER HIGH-SCHOOL
+Subject.create :name => "english", :faculty => "english", :group => "lowerhs"
+Subject.create :name => "english as a second language", :faculty => "english", :group => "lowerhs"
+
+Subject.create :name => "mathematics", :faculty => "mathematics", :group => "lowerhs"
+
+Subject.create :name => "science", :faculty => "science", :group => "lowerhs"
+
+Subject.create :name => "humanities and social science", :faculty => "humanities", :group => "lowerhs"
+
+Subject.create :name => "art", :faculty => "arts", :group => "lowerhs"
+Subject.create :name => "drama", :faculty => "arts", :group => "lowerhs"
+Subject.create :name => "dance", :faculty => "arts", :group => "lowerhs"
+Subject.create :name => "media", :faculty => "arts", :group => "lowerhs"
+Subject.create :name => "music", :faculty => "arts", :group => "lowerhs"
+Subject.create :name => "photography", :faculty => "arts", :group => "lowerhs"
+
+Subject.create :name => "design and technology", :faculty => "business and it", :group => "lowerhs"
+Subject.create :name => "business information technology", :faculty => "business and it", :group => "lowerhs"
+
+Subject.create :name => "home economics", :faculty => "other", :group => "lowerhs"
+Subject.create :name => "physical education", :faculty => "other", :group => "lowerhs"
+Subject.create :name => "health", :faculty => "other", :group => "lowerhs"
+
+
+# faculties
+# Subject.where("faculty like ?", "#{faculty.downcase}")
+# mathematics
+# other
+# business and it
+# arts 
+# science
+# language
+# english humanities
+
--- db/scripts/subject.rb
+++ db/scripts/subject.rb
@@ -1,178 +1,44 @@
 # rails c 
 # load './db/scripts/subject.rb'
 
-# YEAR 12 SUBJECTS
-Subject.create :name => "mathematics applications", :faculty => "mathematics", :group => "year12"
-Subject.create :name => "mathematics essentials", :faculty => "mathematics", :group => "year12"
-Subject.create :name => "mathematics methods", :faculty => "mathematics", :group => "year12"
-Subject.create :name => "mathematics specialist", :faculty => "mathematics", :group => "year12"
+Subject.create :name => "mathematics", :faculty => "mathematics", :group => "highschool"
+Subject.create :name => "specialist", :faculty => "mathematics", :group => "highschool"
 
-Subject.create :name => "accounting and finance", :faculty => "business and it", :group => "year12"
-Subject.create :name => "economics", :faculty => "business and it", :group => "year12"
-Subject.create :name => "applied information technology", :faculty => "business and it", :group => "year12"
-Subject.create :name => "engineering studies mechanical", :faculty => "business and it", :group => "year12"
-Subject.create :name => "career and enterprise", :faculty => "business and it", :group => "year12"
+Subject.create :name => "biology", :faculty => "science", :group => "highschool"
+Subject.create :name => "human biology", :faculty => "science", :group => "highschool"
+Subject.create :name => "chemistry", :faculty => "science", :group => "highschool"
+Subject.create :name => "physics", :faculty => "science", :group => "highschool"
+Subject.create :name => "sport science", :faculty => "science", :group => "highschool"
+Subject.create :name => "engineering", :faculty => "science", :group => "highschool"
+Subject.create :name => "computer programming", :faculty => "science", :group => "highschool"
 
-Subject.create :name => "biology", :faculty => "science", :group => "year12"
-Subject.create :name => "human biology", :faculty => "science", :group => "year12"
-Subject.create :name => "chemistry", :faculty => "science", :group => "year12"
-Subject.create :name => "physics", :faculty => "science", :group => "year12"
-Subject.create :name => "integrated science", :faculty => "science", :group => "year12"
+Subject.create :name => "english", :faculty => "english", :group => "highschool"
+Subject.create :name => "literature", :faculty => "english", :group => "highschool"
 
-Subject.create :name => "english (and foundational english)", :faculty => "english", :group => "year12"
-Subject.create :name => "english as an additional language/dialect", :faculty => "english", :group => "year12"
-Subject.create :name => "literature", :faculty => "english", :group => "year12"
+Subject.create :name => "geography", :faculty => "humanities", :group => "highschool"
+Subject.create :name => "history", :faculty => "humanities", :group => "highschool"
+Subject.create :name => "pyschology", :faculty => "humanities", :group => "highschool"
+Subject.create :name => "politics and law", :faculty => "humanities", :group =>"highschool"
 
-Subject.create :name => "geography", :faculty => "humanities", :group => "year12"
-Subject.create :name => "modern history", :faculty => "humanities", :group => "year12"
-Subject.create :name => "pyschology", :faculty => "humanities", :group => "year12"
-Subject.create :name => "politics and law", :faculty => "humanities", :group =>"year 12"
+Subject.create :name => "art", :faculty => "arts", :group => "highschool"
+Subject.create :name => "drama", :faculty => "arts", :group => "highschool"
+Subject.create :name => "dance", :faculty => "arts", :group => "highschool"
+Subject.create :name => "media", :faculty => "arts", :group => "highschool"
+Subject.create :name => "photography", :faculty => "arts", :group => "highschool"
 
-Subject.create :name => "music", :faculty => "arts", :group => "year12"
-Subject.create :name => "drama", :faculty => "arts", :group => "year12"
-Subject.create :name => "dance", :faculty => "arts", :group => "year12"
+Subject.create :name => "chinese", :faculty => "language", :group => "highschool"
+Subject.create :name => "japanese", :faculty => "language", :group => "highschool"
+Subject.create :name => "italian", :faculty => "language", :group => "highschool"
+Subject.create :name => "spanish", :faculty => "language", :group => "highschool"
+Subject.create :name => "french", :faculty => "language", :group => "highschool"
+Subject.create :name => "german", :faculty => "language", :group => "highschool"
+Subject.create :name => "other", :faculty => "language", :group => "highschool"
 
-Subject.create :name => "chinese background language", :faculty => "language", :group => "year12"
-Subject.create :name => "chinese first language", :faculty => "language", :group => "year12"
-Subject.create :name => "chinese second language", :faculty => "language", :group => "year12"
-Subject.create :name => "french second language", :faculty => "language", :group => "year12"
-Subject.create :name => "german second language", :faculty => "language", :group => "year12"
-Subject.create :name => "japanese second language", :faculty => "language", :group => "year12"
-
-Subject.create :name => "materials design and technology - metal", :faculty => "other", :group => "year12"
-Subject.create :name => "materials design and technology - wood", :faculty => "other", :group => "year12"
-Subject.create :name => "outdoor recreation", :faculty => "other", :group => "year12"
-Subject.create :name => "physical education studies general", :faculty => "other", :group => "year12"
-Subject.create :name => "health studies", :faculty => "other", :group => "year12"
-Subject.create :name => "media production and analysis", :faculty => "other", :group => "year12"
-
-# YEAR 11 SUBJECTS
-Subject.create :name => "mathematics applications", :faculty => "mathematics", :group => "year11"
-Subject.create :name => "mathematics essentials", :faculty => "mathematics", :group => "year11"
-Subject.create :name => "mathematics methods", :faculty => "mathematics", :group => "year11"
-Subject.create :name => "mathematics specialist", :faculty => "mathematics", :group => "year11"
-
-Subject.create :name => "accounting and finance", :faculty => "business and it", :group => "year11"
-Subject.create :name => "economics", :faculty => "business and it", :group => "year11"
-Subject.create :name => "applied information technology", :faculty => "business and it", :group => "year11"
-Subject.create :name => "engineering studies mechanical", :faculty => "business and it", :group => "year11"
-Subject.create :name => "career and enterprise", :faculty => "business and it", :group => "year11"
-
-Subject.create :name => "biology", :faculty => "science", :group => "year11"
-Subject.create :name => "human biology", :faculty => "science", :group => "year11"
-Subject.create :name => "chemistry", :faculty => "science", :group => "year11"
-Subject.create :name => "physics", :faculty => "science", :group => "year11"
-Subject.create :name => "integrated science", :faculty => "science", :group => "year11"
-
-Subject.create :name => "english (and foundational english)", :faculty => "english", :group => "year11"
-Subject.create :name => "english as an additional language/dialect", :faculty => "english", :group => "year11"
-Subject.create :name => "literature", :faculty => "english", :group => "year11"
-
-Subject.create :name => "geography", :faculty => "humanities", :group => "year11"
-Subject.create :name => "modern history", :faculty => "humanities", :group => "year11"
-Subject.create :name => "pyschology", :faculty => "humanities", :group => "year11"
-Subject.create :name => "politics and law", :faculty => "humanities", :group =>"year 11"
-
-Subject.create :name => "music", :faculty => "arts", :group => "year11"
-Subject.create :name => "drama", :faculty => "arts", :group => "year11"
-Subject.create :name => "dance", :faculty => "arts", :group => "year11"
-
-Subject.create :name => "chinese background language", :faculty => "language", :group => "year11"
-Subject.create :name => "chinese first language", :faculty => "language", :group => "year11"
-Subject.create :name => "chinese second language", :faculty => "language", :group => "year11"
-Subject.create :name => "french second language", :faculty => "language", :group => "year11"
-Subject.create :name => "german second language", :faculty => "language", :group => "year11"
-Subject.create :name => "japanese second language", :faculty => "language", :group => "year11"
-
-Subject.create :name => "materials design and technology - metal", :faculty => "other", :group => "year11"
-Subject.create :name => "materials design and technology - wood", :faculty => "other", :group => "year11"
-Subject.create :name => "outdoor recreation", :faculty => "other", :group => "year12"
-Subject.create :name => "physical education studies general", :faculty => "other", :group => "year11"
-Subject.create :name => "health studies", :faculty => "other", :group => "year11"
-Subject.create :name => "media production and analysis", :faculty => "other", :group => "year11"
-Subject.create :name => "food science and technology", :faculty => "other", :group => "year11"
-
-# YEAR 10
-Subject.create :name => "mathematics", :faculty => "mathematics", :group => "year10"
-
-Subject.create :name => "accounting", :faculty => "business and it", :group => "year10"
-Subject.create :name => "law and commerce", :faculty => "business and it", :group => "year10"
-Subject.create :name => "law criminal investigation", :faculty => "business and it", :group => "year10"
-Subject.create :name => "computer game software design", :faculty => "business and it", :group => "year10"
-
-Subject.create :name => "general science", :faculty => "science", :group => "year10"
-Subject.create :name => "medical history", :faculty => "science", :group => "year10"
-Subject.create :name => "biology", :faculty => "science", :group => "year10"
-Subject.create :name => "chemistry", :faculty => "science", :group => "year10"
-Subject.create :name => "physics", :faculty => "science", :group => "year10"
-
-Subject.create :name => "english (and foundation english)", :faculty => "english", :group => "year10"
-Subject.create :name => "english as an additional language", :faculty => "english", :group => "year10"
-Subject.create :name => "literature", :faculty => "english", :group => "year10"
-
-Subject.create :name => "social sciences", :faculty => "humanities", :group => "year10"
-Subject.create :name => "geography", :faculty => "humanities", :group => "year10"
-Subject.create :name => "modern history", :faculty => "humanities", :group => "year10"
-Subject.create :name => "psychology", :faculty => "humanities", :group => "year10"
-Subject.create :name => "politics and law", :faculty => "humanities", :group => "year10"
-
-Subject.create :name => "music", :faculty => "arts", :group => "year10"
-Subject.create :name => "drama", :faculty => "arts", :group => "year10"
-Subject.create :name => "dance", :faculty => "arts", :group => "year10"
-
-Subject.create :name => "chinese background language", :faculty => "language", :group => "year10"
-Subject.create :name => "chinese first language", :faculty => "language", :group => "year10"
-Subject.create :name => "chinese second language", :faculty => "language", :group => "year10"
-Subject.create :name => "french second language", :faculty => "language", :group => "year10"
-Subject.create :name => "german second language", :faculty => "language", :group => "year10"
-Subject.create :name => "japanese second language", :faculty => "language", :group => "year10"
-
-Subject.create :name => "materials design and technology - metal", :faculty => "other", :group => "year10"
-Subject.create :name => "materials design and technology - wood", :faculty => "other", :group => "year10"
-Subject.create :name => "outdoor recreation", :faculty => "other", :group => "year10"
-Subject.create :name => "outdoor education", :faculty => "other", :group => "year10"
-Subject.create :name => "physical education studies general", :faculty => "other", :group => "year10"
-Subject.create :name => "health studies", :faculty => "other", :group => "year10"
-Subject.create :name => "media prodution and analysis", :faculty => "other", :group => "year10"
-Subject.create :name => "food science and technology", :faculty => "other", :group => "year10"
-Subject.create :name => "electronics", :faculty => "other", :group => "year10"
-Subject.create :name => "robotics and electronics", :faculty => "other", :group => "year10"
-Subject.create :name => "film production", :faculty => "other", :group => "year10"
-Subject.create :name => "photography", :faculty => "other", :group => "year10"
-Subject.create :name => "practical engineering", :faculty => "other", :group => "year10"
-
-# LOWER HIGH-SCHOOL
-Subject.create :name => "english", :faculty => "english", :group => "lowerhs"
-Subject.create :name => "english as a second language", :faculty => "english", :group => "lowerhs"
-
-Subject.create :name => "mathematics", :faculty => "mathematics", :group => "lowerhs"
-
-Subject.create :name => "science", :faculty => "science", :group => "lowerhs"
-
-Subject.create :name => "humanities and social science", :faculty => "humanities", :group => "lowerhs"
-
-Subject.create :name => "art", :faculty => "arts", :group => "lowerhs"
-Subject.create :name => "drama", :faculty => "arts", :group => "lowerhs"
-Subject.create :name => "dance", :faculty => "arts", :group => "lowerhs"
-Subject.create :name => "media", :faculty => "arts", :group => "lowerhs"
-Subject.create :name => "music", :faculty => "arts", :group => "lowerhs"
-Subject.create :name => "photography", :faculty => "arts", :group => "lowerhs"
-
-Subject.create :name => "design and technology", :faculty => "business and it", :group => "lowerhs"
-Subject.create :name => "business information technology", :faculty => "business and it", :group => "lowerhs"
-
-Subject.create :name => "home economics", :faculty => "other", :group => "lowerhs"
-Subject.create :name => "physical education", :faculty => "other", :group => "lowerhs"
-Subject.create :name => "health", :faculty => "other", :group => "lowerhs"
-
-
-# faculties
-# Subject.where("faculty like ?", "#{faculty.downcase}")
-# mathematics
-# other
-# business and it
-# arts 
-# science
-# language
-# english humanities
-
+Subject.create :name => "guitar", :faculty => "music", :group => "highschool"
+Subject.create :name => "piano", :faculty => "music", :group => "highschool"
+Subject.create :name => "violin", :faculty => "music", :group => "highschool"
+Subject.create :name => "flute", :faculty => "music", :group => "highschool"
+Subject.create :name => "singing", :faculty => "music", :group => "highschool"
+Subject.create :name => "trumpet", :faculty => "music", :group => "highschool"
+Subject.create :name => "drums", :faculty => "music", :group => "highschool"
+Subject.create :name => "other", :faculty => "music", :group => "highschool"
