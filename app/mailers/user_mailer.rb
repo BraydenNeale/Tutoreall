a4a7861f35c201f46d92cedb92fb4b1e05ce2bcb
Brayden Neale <brayden.neale@gmail.com>
2016-01-21 20:53:39 +0800

    Updated lesson mailer logic - and added to controller to send

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,14 @@
 class UserMailer < ApplicationMailer
 
-  def new_lesson_email(student, tutor, subject, lesson)
-    @student = student
-    @tutor = tutor
-    @subject = subject
-    @date = date
+  def new_lesson_email(lesson)
+    @student = lesson.student
+    @tutor = lesson.tutor
+    @subject = lesson.subject
+    @date = lesson.date
     @url = 'http://www.tutorial.academy'
 
     subject = 'A student has requested a lesson from you'
-    
+
     # Tutor has set up the lesson - so email the student (else - opposite)
     if current_user.is_a? Tutor
       subject = 'A tutor has set up a lesson with you'
@@ -18,11 +18,11 @@ class UserMailer < ApplicationMailer
     end
   end
 
-  def lesson_change_email(student, tutor, subject)
-    @student = student
-    @tutor = tutor
-    @subject = subject
-    @date = date
+  def lesson_change_email(lesson)
+    @student = lesson.student
+    @tutor = lesson.tutor
+    @subject = lesson.subject
+    @date = lesson.date
     @url = 'http://www.tutorial.academy'
 
     subject = 'The details of one of your scheduled lessons have changed'
