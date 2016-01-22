ad4bf51ad0484dd490955d56d4da9a43450b55ff
Brayden Neale <brayden.neale@gmail.com>
2016-01-22 22:12:15 +0800

    Updated email subjects - lessons/signups/messages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,11 +9,11 @@ class UserMailer < ApplicationMailer
     @lesson = lesson
     @url = 'http://www.tutorial.academy'
 
-    subject = 'A student has requested a lesson from you'
+    subject = 'Tutorial Academy - A student has requested a lesson from you'
 
     # Tutor has set up the lesson - so email the student (else - opposite)
     if @current.is_a? Tutor
-      subject = 'A tutor has set up a lesson with you'
+      subject = 'Tutorial Academy - A tutor has set up a lesson with you'
       mail(to: @student.email, subject: subject)
     else
       mail(to: @tutor.email, subject: subject)
@@ -29,7 +29,7 @@ class UserMailer < ApplicationMailer
     @lesson = lesson
     @url = 'http://www.tutorial.academy'
 
-    subject = 'The details of one of your scheduled lessons have changed'
+    subject = 'Tutorial Academy - The details of one of your scheduled lessons have changed'
     to = @tutor
     if @current.is_a? Tutor
       to = @student
