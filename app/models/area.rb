e94e73ab250df45a61fba69ea6b48a7167b1d701
Brayden Neale <brayden.neale@gmail.com>
2016-04-04 23:27:33 +0800

    geocoder function added to make sure lat/long appears in Perth area

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,10 +2,14 @@ class Area < ActiveRecord::Base
 		validates :name, presence: true, uniqueness: true
 		validates :code, presence: true, uniqueness: true
 
-    geocoded_by :name
+    geocoded_by :get_gmap_address
     after_validation :geocode
 
 	def display_name
     return "#{self.name}".titleize
+  end
+
+  def get_gmap_address
+    return "#{self.name}, #{self.state}"
   end
 end
